3e5d2b7ba8172ff83a844aa019386b6203d1e219
Kawakami Haruya <haruya20021126@gmail.com>
2026-01-29 19:04:50 +0900

    first commit

---
+++ Views/Scan/DocumentScannerView.swift
@@ -0,0 +1,52 @@
+//
+//  DocumentScannerView.swift
+//  ReportScan
+//
+//  Created by Claude Code on 2026/01/29.
+//
+
+import SwiftUI
+import VisionKit
+
+struct DocumentScannerView: UIViewControllerRepresentable {
+    @Binding var scannedImages: [UIImage]
+    @Environment(\.dismiss) var dismiss
+
+    func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
+        let scanner = VNDocumentCameraViewController()
+        scanner.delegate = context.coordinator
+        return scanner
+    }
+
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
+        let parent: DocumentScannerView
+
+        init(_ parent: DocumentScannerView) {
+            self.parent = parent
+        }
+
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+            var images: [UIImage] = []
+            for i in 0..<scan.pageCount {
+                images.append(scan.imageOfPage(at: i))
+            }
+            parent.scannedImages = images
+            parent.dismiss()
+        }
+
+        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+            parent.dismiss()
+        }
+
+        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+            print("Scanner error: \(error.localizedDescription)")
+            parent.dismiss()
+        }
+    }
+}
